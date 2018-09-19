6962169131c0d30a80363b66781f10c473a292e0
Jonathan Gallo <jonnygallo@gmail.com>
2018-09-19 15:05:40 -0700

    completed display dummy dtata

---
+++ src/components/list.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import dummyListData from '../dummy_data/list_data';
+
+class List extends Component {
+    state = {
+        list:[]
+    }
+
+    componentDidMount() {
+        this.getListData();
+    }
+
+    getListData () {
+
+        this.setState ({
+            list: dummyListData
+        })
+    }
+
+    render () {
+        const listElements = this.state.list.map((item,index)=>{
+            return <li className="collection-item" key={item._id}>{item.title}</li>
+        })
+        return (
+            <ul className="collection">
+                {listElements}
+            </ul>
+        );
+    }
+
+}
+
+export default List;
---
+++ src/dummy_data/list_data.js
@@ -0,0 +1,27 @@
+export default [
+    {
+        title:'Go to store',
+        datails: 'get eggs, milk, bread, and diet coke',
+        _id: '001'      
+    },
+    {
+        title:'study React',
+        datails: 'go over React notes for class',
+        _id: '002'      
+    },
+    {
+        title:'Walk Harley',
+        datails: 'take harley for a walk when you get home',
+        _id: '003'      
+    },
+    {
+        title:'Wash Truck',
+        datails: 'go through carwash',
+        _id: '004'      
+    },
+    {
+        title:'Workout',
+        datails: 'workout for 30 mins',
+        _id: '005'      
+    }
+];
